c07af1bfeb26614fd245248052f985f5d3354140
Zach-Sisson-1 <zach.r.sisson@gmail.com>
2022-03-03 11:07:44 -0800

    branched script for removing itertools

--- motif-mark-oop.py
+++ motif-mark-oop.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
 import argparse
-import itertools
+from distutils.command.install_lib import PYTHON_SOURCE_EXTENSION
+import re
+from urllib.request import parse_http_list
 
 def get_args():
-	parser = argparse.ArgumentParser(description="XXXXXXX")
+	parser = argparse.ArgumentParser(description="XXXX")
 	parser.add_argument("-f", "--fasta_file", help = "Path to the input Fasta File", type=str, required=True)
 	parser.add_argument("-m", "--motifs_file", help = "Path to the input Motifs file", type=str, required=True)
 	return parser.parse_args()
@@ -12,7 +14,7 @@ def get_args():
 args = get_args()
 
 
-#Initalize class for Genes:
+#Initalize class for Motifs:
 
 class motif:
 	"""
@@ -25,6 +27,7 @@ class motif:
 		self.motif_list_of_lists = [] # list of lists that will be used to generate all possible motif combinations
 		self.motif_combinations_pre = [] 
 		self.motif_combinations = [] # list that will contain all possible nucleotide sequences given motif
+		
 		#Read each nucleotide in the motif and append the appropriate list of possible nucleotides
 		for nucleotide in self.motif:
 			if nucleotide == "r":
@@ -40,49 +43,109 @@ class motif:
 		#Join together the dictionaries, creating the strings of nucelotides
 		for i in self.motif_combinations_pre:
 			string = ""
-			self.motif_combinations.append(string.join(i)) 
+			self.motif_combinations.append(string.join(i))
 
-#read in motif file, creating objects for motif
-motif_dict ={} #dict to hold input motifs and their class objects
+	def gene_search(self,Gene):
+		"""
+		Takes in a gene object and searches for motifs in the gene, returning three dictionaries, 
+		each containing the unique motifs as keys and the matching locations as values.
+		""" 
+		#initialize dicts which will house motifs as keys and location hits as values
+		pre_exon_dict = {} 
+		exon_dict = {}
+		post_exon_dict = {}
+		
+		pre_exon = Gene.pre_exon
+		exon = Gene.exon
+		post_exon = Gene.post_exon
+
+		#Create iterobjects for each motif combination which locates all the motif hits for the given sequence
+		for seq in self.motif_combinations:
+			pre_exon_hits = re.finditer(seq,pre_exon) 
+			exon_hits = re.finditer(seq,exon)
+			post_exon_hits = re.finditer(seq,post_exon)
+
+			#Next add motif with its hits to dict, or "n/a" if none found
+			if len(pre_exon_hits) != 0:
+				pre_exon_dict[seq] = pre_exon_hits
+			else:
+				pre_exon_dict[seq] = "n/a"
+			if len(exon_hits) != 0:
+				exon_dict[seq] = exon_hits
+			else:
+				exon_dict[seq] = "n/a"
+			if len(post_exon_hits) != 0:
+				post_exon_dict[seq] = post_exon_hits
+			else:
+				post_exon_dict[seq] = "n/a"
+		return pre_exon_dict, exon_dict, post_exon_dict
+
+
+class Gene:
+	"""
+	Gene class that takes in a gene pattern and stores all exons, pre-exons and post-exons.
+	"""
+	def __init__(self,gene_seq):
+		self.gene_seq = gene_seq
+		self.pre_exon = ""  
+		self.exon = ""
+		self.post_exon = ""	
+
+	#Parse gene_seq into pre-exon, exon, and postexon
+		self.pre_exon = re.findall('[a-z]+',self.gene_seq)[0]
+		self.post_exon = re.findall('[a-z]+',self.gene_seq)[1]
+		self.exon = re.findall('[A-Z]+',self.gene_seq)
+
+
+
+#read in motif file, creating a dict to hold motif objects
+motif_dict = {} #dict to hold input motifs and their class objects
 with open(args.motifs_file, "r") as motif_file:
 	for line in motif_file:
 		Motif = line.strip('\n')
 		motif_dict[Motif] = motif(str(Motif))
 
+####working on this#########
+#Read in fasta file, creating a dict to hold gene objects
+with open(args.fasta_file, "r") as fasta_file:
+	gene_dict = {} #empty dict that will contain fasta headers and corresponding genes
+	working_seq =""
 
+	for line in fasta_file:
+		if line[0] == '>':
+			new_key = line[]
+			gene_dict[old_key] = working_seq 
+			old_key = line[1:]      
+			working_seq = ""
 
-#Test
-for key,value in motif_dict.items():
-	print(value.motif_combinations)
+		else:
+			working_seq += line.strip('\n')
 
-
-# class Gene:
-# 	def __init__(self,intron,exon):
-# 		"""Gene class to contain intron and exons """
-# 		self.intron = intron
-# 		self.	
-
-
-
-
-
-
-
-
-# #Read in fasta file, making a gene object for each entry
-# with open(args.fasta_file, "r") as fasta_file:
-# 	gene_dict = {} #empty dict that will contain fasta headers and corresponding genes
-# 	for line in fasta_file:
-# 		if line[0] == '>':      #new entry = bank the previous sequence as a gene object and reset
-# 			gene_dict[]= ""   #set empty string for header value
-#  			working_seq = ""
-# 		else:
-# 			working_seq += line.strip('\n')
+for key,value in gene_dict.items():
+	print(key)
+	print('\n')
+	print(value)
+	print('\n')
 
 		
 
+class Drawing:
+	"""
+	Drawing class which will take in X and output a 
+	pycairo drawing with introns,exons, and motifs labeled. 
+	"""
+	#takes in gene
+	#takes in motif
+	#draws 
 
 
+
+	
+
+
+#Still need to parse Fafsa file
+#Still need to create drawing class that inputs something and creates drawings for pycairo.
+	
 
 
 
